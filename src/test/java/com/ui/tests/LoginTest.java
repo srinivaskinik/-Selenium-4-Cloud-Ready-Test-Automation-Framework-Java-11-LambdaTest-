881a3945f23ffcf9fc8148826d1660c29aa3d004
srinivas kalmady <srinivaskinik2@gmail.com>
2025-09-05 17:26:36 +0530

    changes to the file name

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -23,15 +23,15 @@ public class LoginTest extends TestBase{
 		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(), "Srinivas K");
 	}
 	
-//	@Test(description = "Verifies if the valid user is able to login into the application",groups = {"e2e","sanity"},dataProviderClass = com.ui.dataproviders.LoginDataProvider.class,dataProvider="LoginTestCSVDataProvider")
-//	public void loginCSVTest(User user) {
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(), "Srinivas K");
-//	}
-//	
-//	@Test(description = "Verifies if the valid user is able to login into the application",groups = {"e2e","sanity"},dataProviderClass = com.ui.dataproviders.LoginDataProvider.class,dataProvider="LoginTestExcelDataProvider",retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
-//	public void loginExcelTest(User user) {
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(), "Srinivas K1");
-//	}
+	@Test(description = "Verifies if the valid user is able to login into the application",groups = {"e2e","sanity"},dataProviderClass = com.ui.dataproviders.LoginDataProvider.class,dataProvider="LoginTestCSVDataProvider")
+	public void loginCSVTest(User user) {
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(), "Srinivas K");
+	}
+	
+	@Test(description = "Verifies if the valid user is able to login into the application",groups = {"e2e","sanity"},dataProviderClass = com.ui.dataproviders.LoginDataProvider.class,dataProvider="LoginTestExcelDataProvider",retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
+	public void loginExcelTest(User user) {
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(), "Srinivas K1");
+	}
 
 
 }
